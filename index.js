222d863ff95bb56a77f685295b155e156c239e95
sarahmibrahim <93496519+sarahmibrahim@users.noreply.github.com>
2021-12-14 10:30:14 +0000

    nothing

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-console.log ("hello")
-const express = require('express')
-const app = express()
-const port = 3000
-const router = require('./router')
+console.log("hello");
+const express = require("express");
+const app = express();
+const port = 3000;
+const router = require("./router");
 
-app.use(express.json())
-app.use(router)
+app.use(express.json());
+app.use(router);
 
-
-app.listen(port, () =>{
-    console.log(`example app listening on http://localhost: {port}`)
-})
+app.listen(port, () => {
+  console.log(`example app listening on http://localhost: {port}`);
+});
