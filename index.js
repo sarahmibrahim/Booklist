511955fe572bc735dd068f6e72fcc2e9bf7e71a4
Laura Rose Mary Hansen <laurarosemaryhansen@gmail.com>
2021-12-14 11:31:50 +0000

    dunno

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log("hello");
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const router = require("./router");
 
 app.use(express.json());
