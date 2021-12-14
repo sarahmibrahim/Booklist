2220ecd921e27f54a28bc87309c597ba56f1dc8d
Laura Rose Mary Hansen <laurarosemaryhansen@gmail.com>
2021-12-14 11:59:18 +0000

    pls work

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
+
 console.log ("hello")
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 const router = require('./router')
-=======
-console.log("hello");
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
-const router = require("./router");
->>>>>>> 511955fe572bc735dd068f6e72fcc2e9bf7e71a4
 
 app.use(express.json());
 app.use(router);
