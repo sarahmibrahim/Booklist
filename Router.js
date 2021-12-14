7ada51be38c5da6401bf9a30d22dc3d1e0015a4d
sarahmibrahim <93496519+sarahmibrahim@users.noreply.github.com>
2021-12-14 14:39:09 +0000

    Co-authored-by: compinschools <compinschools@users.noreply.github.com>

--- Router.js
+++ Router.js
@@ -1,13 +1,14 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const books = require('./bookController')
+const books = require("./BookController");
 
-router.put('/book/:id',books.update)
-router.delete('/book/:id',books.delete)
+router.put("/book/:id", books.update);
+router.delete("/book/:id", books.delete);
 
-router.get('/book', books.index)
-router.post('/book/create', books.create)
-router.get('/book/:id', books.show)
-
-
+router.get("/book", books.index);
+router.post("/book/create", books.create);
+router.get("/book/:id", books.show);
+router.get("/author/:author", books.showauthor);
+router.get("/Title/:Title", books.showTitle);
+router.get("/status/:status", books.showstatus);
 module.exports = router;
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 console.log("hello");
 const express = require("express");
 const app = express();
-const port = 3000;
-const router = require("./router");
+const port = process.env.PORT || 3000;
+const router = require("./Router");
 
 app.use(express.json());
 app.use(router);
 
 app.listen(port, () => {
-  console.log(`example app listening on http://localhost: {port}`);
+  console.log(`example app listening on http://localhost: ${port}`);
 });
